03254c92f63c5fc3e66ce8f727ebf6667bf800de
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2025-11-04 09:58:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/gouthamkl16.git /TelegramBot
+  git clone https://github.com/Sridhark01.git /DQ-BULLET-V3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TelegramBot
+  git clone $UPSTREAM_REPO /DQ-BULLET-V3
 fi
-cd /TelegramBot
+cd /DQ-BULLET-V3
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜....🍁"
 python3 bot.py
