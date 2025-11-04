e5a63186ac3813b128eea465f0b56fb98ad14093
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2025-11-04 18:27:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,6 +59,11 @@ class Bot(Client):
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
         logging.info(script.LOGO)
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
         print("Heheh Funzzzzz only😀")
         
         client = webserver.AppRunner(await bot_run())
